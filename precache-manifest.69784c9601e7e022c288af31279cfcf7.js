0f0490b9f0d6bb3bd0fc9e8dd3dea45d95e476ee
Pavel-Lunin <62575772+Pavel-Lunin@users.noreply.github.com>
2020-09-30 20:54:12 +1000

    Updates

---
+++ precache-manifest.69784c9601e7e022c288af31279cfcf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cd10e1421c3068a7583b002289d6576",
+    "url": "/index.html"
+  },
+  {
+    "revision": "595549738abb33936f5b",
+    "url": "/static/css/main.093e7d0d.chunk.css"
+  },
+  {
+    "revision": "913fc59434c7ea3a1676",
+    "url": "/static/js/2.eb18cc35.chunk.js"
+  },
+  {
+    "revision": "e33bd41e3cbb3743b58dfbc09e761595",
+    "url": "/static/js/2.eb18cc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595549738abb33936f5b",
+    "url": "/static/js/main.94431d34.chunk.js"
+  },
+  {
+    "revision": "82af36e83412180715d7",
+    "url": "/static/js/runtime-main.86c19a1b.js"
+  },
+  {
+    "revision": "1a4634681b3321ac8bac434c148ade2d",
+    "url": "/static/media/NewsScreen.1a463468.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/test-task-news/precache-manifest.f71d27c8b41f25b0b1a4d5ed91decf4f.js"
+  "/precache-manifest.69784c9601e7e022c288af31279cfcf7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-task-news/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
