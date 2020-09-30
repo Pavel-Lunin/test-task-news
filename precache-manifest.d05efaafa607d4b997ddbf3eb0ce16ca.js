08d4404048246169f4a404e0bfec9dd978b927b3
Pavel-Lunin <62575772+Pavel-Lunin@users.noreply.github.com>
2020-09-30 15:08:28 +1000

    Updates

---
+++ precache-manifest.d05efaafa607d4b997ddbf3eb0ce16ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b939b53d57bc5986be7788b65c73f0ed",
+    "url": "/demo-news/index.html"
+  },
+  {
+    "revision": "dafde33aae27a50dd6c5",
+    "url": "/demo-news/static/css/main.093e7d0d.chunk.css"
+  },
+  {
+    "revision": "dfc1b7d8da167d458b17",
+    "url": "/demo-news/static/js/2.cde6789f.chunk.js"
+  },
+  {
+    "revision": "e33bd41e3cbb3743b58dfbc09e761595",
+    "url": "/demo-news/static/js/2.cde6789f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafde33aae27a50dd6c5",
+    "url": "/demo-news/static/js/main.ccdd6da5.chunk.js"
+  },
+  {
+    "revision": "61bd503ad7860b668994",
+    "url": "/demo-news/static/js/runtime-main.4f75afe9.js"
+  },
+  {
+    "revision": "1a4634681b3321ac8bac434c148ade2d",
+    "url": "/demo-news/static/media/NewsScreen.1a463468.jpg"
+  }
+]);
