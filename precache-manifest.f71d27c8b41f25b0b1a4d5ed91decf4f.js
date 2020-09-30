13d1c957ec83125b2a97d6e94923d276c5709242
Pavel-Lunin <62575772+Pavel-Lunin@users.noreply.github.com>
2020-09-30 21:08:14 +1000

    Updates

---
+++ precache-manifest.f71d27c8b41f25b0b1a4d5ed91decf4f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf8ec3bde7ce39a75f2ef26ec61492f",
+    "url": "/test-task-news/index.html"
+  },
+  {
+    "revision": "595549738abb33936f5b",
+    "url": "/test-task-news/static/css/main.093e7d0d.chunk.css"
+  },
+  {
+    "revision": "913fc59434c7ea3a1676",
+    "url": "/test-task-news/static/js/2.eb18cc35.chunk.js"
+  },
+  {
+    "revision": "e33bd41e3cbb3743b58dfbc09e761595",
+    "url": "/test-task-news/static/js/2.eb18cc35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595549738abb33936f5b",
+    "url": "/test-task-news/static/js/main.94431d34.chunk.js"
+  },
+  {
+    "revision": "eeac57d70047e0a5adde",
+    "url": "/test-task-news/static/js/runtime-main.55cb495c.js"
+  },
+  {
+    "revision": "1a4634681b3321ac8bac434c148ade2d",
+    "url": "/test-task-news/static/media/NewsScreen.1a463468.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/newsprecache-manifest.882c3c5863f227a8343ca98688fc29c8.js"
+  "/test-task-news/precache-manifest.f71d27c8b41f25b0b1a4d5ed91decf4f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/newsindex.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-task-news/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
